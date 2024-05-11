b0fdd5a0fcf961916491baf0fc5a5a692baf01ae
moerpel <66588833+moerpel@users.noreply.github.com>
2024-05-11 20:22:32 +0200

    Create manifest.py

---
+++ manifest.py
@@ -0,0 +1,10 @@
+json
+Copy code
+{
+    "domain": "EV Charger",
+    "name": "SMA EV Charger",
+    "documentation": "https://github.com/moerpel/HA-SMA_EVCharger/readme.md",
+    "dependencies": [],
+    "codeowners": ["moerpel"],
+    "version": "0.1"
+}
