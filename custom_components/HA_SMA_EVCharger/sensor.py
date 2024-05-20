cec13701d49f9a1cc04f61c41a0cc9fae09ced3b
moerpel <66588833+moerpel@users.noreply.github.com>
2024-05-20 18:31:45 +0200

    Fill sensor.py

--- custom_components/HA_SMA_EVCharger/sensor.py
+++ custom_components/HA_SMA_EVCharger/sensor.py
@@ -1,1 +1,55 @@
+import voluptuous as vol
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+import homeassistant.helpers.config_validation as cv
+from datetime import datetime
+import requests
+import json
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
+# Definition der Plattformkonfiguration
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+})
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Setup sensor platform."""
+    coordinator = hass.data["HA_SMA_EVCharger"]
+    add_entities([HA_SMA_EVChargerSensor(coordinator, config[CONF_USERNAME], config[CONF_PASSWORD])])
+
+class HA_SMA_EVChargerSensor(CoordinatorEntity, SensorEntity):
+    """Definition des HA_SMA_EVCharger Sensors."""
+
+    def __init__(self, coordinator, username, password):
+        """Initialisieren des Sensors."""
+        super().__init__(coordinator)
+        self.coordinator = coordinator
+        self._username = username
+        self._password = password
+        self._state = None
+        self._attributes = {}
+
+    @property
+    def name(self):
+        """Rückgabe des Namens des Sensors."""
+        return "HA_SMA_EVCharger Sensor"
+
+    @property
+    def state(self):
+        """Rückgabe des aktuellen Sensorzustands."""
+        # Beispiel zur Verwendung der Daten; spezifische Logik hier anpassen
+        if self.coordinator.data:
+            return float(self.coordinator.data[14]['values']) / 1000 if self.coordinator.data[14]['values'] else None
+        return None
+
+    @property
+    def extra_state_attributes(self):
+        """Rückgabe zusätzlicher Attribute des Sensors."""
+        # Weitere nützliche Informationen als Attribute hinzufügen
+        if self.coordinator.data:
+            return {
+                'aktuelle_ladung': float(self.coordinator.data[0]['values']) / 1000 if self.coordinator.data[0]['values'] else None,
+                'zeitstempel': datetime.now().isoformat()
+            }
+        return {}
