7bc019322caab2579f8784ea7a8943c3911b3189
moerpel <66588833+moerpel@users.noreply.github.com>
2024-06-01 12:48:13 +0200

    reworked evcharger_requests.py

--- custom_components/HA_SMA_EVCharger/evcharger_requests.py
+++ custom_components/HA_SMA_EVCharger/evcharger_requests.py
@@ -11,7 +11,8 @@ class EvChargerAPI:
         self.username = username
         self.password = password
         self.session = requests.Session()
-        self.token = None
+        self.access_token = None
+        self.refresh_token = None
 
     def authenticate(self):
         """Authenticate and obtain a token for subsequent API calls."""
@@ -26,15 +27,28 @@ class EvChargerAPI:
         response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
 
         tokens = response.json()
-        self.token = tokens.get("access_token")
+        self.access_token = tokens.get("access_token")
+        _LOGGER.debug(f"Access Token: {self.access_token}")
+        self.refresh_token = tokens.get("refresh_token")
+        _LOGGER.debug(f"Refresh Token: {self.refresh_token}")
 
-        if not self.token:
+        if not self.access_token:
             raise ValueError("Authentication failed, no token received")
 
         # Update session headers with the received token
-        self.session.headers.update({"Authorization": f"Bearer {self.token}"})
+        self.session.headers.update({"Authorization": f"Bearer {self.access_token}"})
 
     def get_data(self):
+        """Refresh token to have a valid authentication."""
+        _LOGGER.info(f"Fetching data from wallbox {self.api_url}.")
+        refresh_payload = {
+            'grant_type': 'refresh_token',
+            'refresh_token': self.refresh_token
+        }
+        token_url = f"{self.api_url}/api/v1/token"
+        response = self.session.post(token_url, data=refresh_payload)
+        _LOGGER.info(f"Result: {response}")
+        _LOGGER.debug(f"Refresh Token Used: {self.refresh_token}")
         """Fetch the data from the API."""
         measurements_url = f"{self.api_url}/api/v1/measurements/live/"
         payload = json.dumps([{"componentId": "IGULD:SELF"}])
@@ -44,12 +58,17 @@ class EvChargerAPI:
         data = response.json()
 
         # Log the entire response for debugging
-        _LOGGER.debug(f"API response: {json.dumps(data, indent=2)}")
+        #_LOGGER.debug(f"API response: {json.dumps(data, indent=2)}")
 
         # Data Processing
         evcharger_current_power = None
         evcharger_total_energy = None
         evcharger_connection_status = None
+        evcharger_mode_switch = None
+        evcharger_mode = None
+        evcharger_charging_status_raw = None
+        evcharger_charging_status = None
+        evcharger_energy_counter__ChargingStation_total_Wh = None
 
         for item in data:
             channel = item['channelId']
@@ -59,13 +78,16 @@ class EvChargerAPI:
             if not isinstance(time_and_value, list) or len(time_and_value) == 0:
                 _LOGGER.error(f"Unexpected data format: {time_and_value}")
                 continue
-
+            #print(channel)
             # Process the latest measurement in time_and_value
             latest_measurement = time_and_value[-1]
+            #print(latest_measurement)
             datetime_str = latest_measurement.get('time')
+            #print(datetime_str)
             value = latest_measurement.get('value')
+            #print(value)
 
-            if datetime_str is None or value is None:
+            ''' if datetime_str is None or value is None:
                 _LOGGER.warning(f"Skipping incomplete measurement: {latest_measurement}")
                 continue
 
@@ -74,25 +96,53 @@ class EvChargerAPI:
                 value = float(value)
             except ValueError as e:
                 _LOGGER.error(f"Error parsing values: {e}")
-                continue
+                continue '''
 
             if channel == "Measurement.ChaSess.WhIn":
                 evcharger_total_energy = value / 1000  # Convert to kWh
             elif channel == "Measurement.Metering.GridMs.TotWIn":
                 evcharger_current_power = value / 1000  # Convert to kW
-            elif channel == "Measurement.Wl.ConnStt":
+            elif channel == "Measurement.Operation.EVeh.Health":
                 evcharger_connection_status = value   # Connection Status
-
-            # Log the parsed values for debugging
-            _LOGGER.debug(f"Channel: {channel}, Current Power: {evcharger_current_power}, Total Energy: {evcharger_total_energy}, Connection Status: {evcharger_connection_status}")
-
-        if evcharger_current_power is None or evcharger_total_energy is None:
+            elif channel == "Measurement.Chrg.ModSw":
+                evcharger_mode_switch = value   # Mode Switch
+            elif channel == "Measurement.Operation.EVeh.ChaStt":
+                evcharger_charging_status_raw = value   # Charging Status
+            elif channel == "Measurement.Metering.GridMs.TotWhIn.ChaSta":
+                evcharger_energy_counter__ChargingStation_total_Wh = value / 1000 # Energy Counter overall charging station Convert to kWh
+        
+        if evcharger_current_power is None or evcharger_total_energy is None or evcharger_connection_status is None or evcharger_mode_switch is None:
             _LOGGER.error("Failed to retrieve necessary data from the API response")
             _LOGGER.debug(f"Final parsed values - Current Power: {evcharger_current_power}, Total Energy: {evcharger_total_energy}, Connection Status: {evcharger_connection_status}")
             raise ValueError("Failed to retrieve necessary data from the API response")
+        # Log the parsed values for debugging
+        _LOGGER.debug(f"Channel: {channel}, Current Power: {evcharger_current_power}, Total Energy: {evcharger_total_energy}, Connection Status: {evcharger_connection_status}, Mode Switch: {evcharger_mode_switch}")
+        
+        #Translations
+        if str(evcharger_mode_switch) == "4950":
+            evcharger_mode ="intelligent charging"    
+        elif str(evcharger_mode_switch) == "4718":
+            evcharger_mode ="fast charging" 
+        else: evcharger_mode = "unknown"
+
+        _LOGGER.debug(f"Translated evcharger switch position raw: {evcharger_mode_switch} to readable: {evcharger_mode}")
+        
+        if str(evcharger_charging_status_raw) == "200111":
+            evcharger_charging_status ="not connected"    
+        elif str(evcharger_charging_status_raw) == "200112":
+            evcharger_charging_status = "sleeping"
+        elif str(evcharger_charging_status_raw) == "200113":
+                evcharger_charging_status = "charging"
+        else: evcharger_charging_status = "unknown"
+        _LOGGER.debug(f"Translated evcharger charging status raw: {evcharger_charging_status_raw} to readable: {evcharger_charging_status}")
 
         return {
             "evcharger_current_power": evcharger_current_power,
             "evcharger_total_energy": evcharger_total_energy,
-            "evcharger_connection_status": evcharger_connection_status
+            "evcharger_connection_status": evcharger_connection_status,
+            "evcharger_mode_switch": evcharger_mode_switch,
+            "evcharger_mode": evcharger_mode,
+            "evcharger_charging_status_raw": evcharger_charging_status_raw,
+            "evcharger_charging_status": evcharger_charging_status,
+            "evcharger_energy_counter__ChargingStation_total_Wh": evcharger_energy_counter__ChargingStation_total_Wh
         }
